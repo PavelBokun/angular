cfa6eca4102d6c29cf85d464554603b555c82d74
PavelBokun <pavel.snabteh@gmail.com>
2024-02-13 22:29:49 +0300

    13.02

--- src/app/parent/child/child.component.ts
+++ src/app/parent/child/child.component.ts
@@ -1,32 +1,29 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Address } from '../parent.component';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-
- export interface Learn{
-  math: number
-  physic: number
-}
 @Component({
   selector: 'app-child',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
-name='Victor'
+inputLearn?:string=""
 
-@Input() sureNameProps?: string
 
-@Input() address?: Address
-
-@Output() setChange = new EventEmitter<Learn>()
+@Output() setChange = new EventEmitter<any>()
 setNumberChange(){
-  const math=5
-  const physic=4
-  this.setChange.emit({math,physic})
+  
+  this.setChange.emit(this.inputLearn)
+}
+
+addItem(){
+  
+  this.inputLearn=""
+}
 }
 
 
-}
 
--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,36 +1,28 @@
 import { Component } from '@angular/core'
-import { ChildComponent, Learn } from './child/child.component'
-
-export interface Address{
-  sity: string
-    street: string
-    house: number
-}
+import { ChildComponent } from './child/child.component'
+import { CommonModule} from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [ChildComponent],
+  imports: [ChildComponent,CommonModule,FormsModule],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css',
 })
 
 export class ParentComponent {
-  name = 'Ivan'
-  sureName = 'Petrov'
-  address : Address= {
-    sity: 'Minsk',
-    street: 'Lenina',
-    house: 49,
-  }
+ 
+  grades: string []=['math: 5','english:3','math: 5','english:3']
+new:any =[]
 
-  math:number
-  physic:number
+  getChange(value:string){
+   debugger
+// this.grades.push(value)
+this.grades.push(value)
 
-  getChange(value:Learn){
-    debugger
-this.physic=value.physic
-this.math=value.math
+console.log(this.grades)
+
 
   }
 }
